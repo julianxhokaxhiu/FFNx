5f133454eca4395090fd532ca707c9785d0f7927
myst6re <myst6re@gmail.com>
2020-04-27 17:18:02 +0200

    Adding Winamp plugin support (FF7Music will be obsolete after this)

---
+++ src/winamp/music.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <windows.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <dsound.h>
+
+#include "../log.h"
+#include "../types.h"
+
+void winamp_music_init();
+void winamp_play_music(char* midi, uint id);
+void winamp_stop_music();
+void winamp_cross_fade_music(char* midi, uint id, uint time);
+void winamp_pause_music();
+void winamp_resume_music();
+bool winamp_music_status();
+void winamp_set_master_music_volume(uint volume);
+void winamp_set_music_volume(uint volume);
+void winamp_set_music_volume_trans(uint volume, uint step);
+void winamp_set_music_tempo(unsigned char tempo);
+void winamp_music_cleanup();
