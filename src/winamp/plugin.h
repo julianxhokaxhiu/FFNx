7250e98c27f5ed8a4a496079185788e55254882e
myst6re <myst6re@gmail.com>
2020-04-28 04:57:19 +0200

    Removing out_wave.dll dependency for winamp plugin.

--- src/winamp/plugin.h
+++ src/winamp/plugin.h
@@ -1,5 +1,7 @@
 #include "windows.h"
 #include "winamp.h"
+#include "../types.h"
+#include <DSound.h>
 
 class WinampPlugin {
 private:
@@ -19,7 +21,7 @@ public:
 	bool open(char* libFileName);
 	void close();
 };
-
+/*
 class WinampOutPlugin : public WinampPlugin {
 private:
 	WinampOutModule* mod;
@@ -34,19 +36,54 @@ public:
 	inline WinampOutModule* getModule() const {
 		return mod;
 	}
+};*/
+
+class CustomOutPlugin {
+private:
+	WinampOutModule mod;
+	static IDirectSoundBuffer* sound_buffer;
+	static uint sound_buffer_size;
+	static uint sound_write_pointer;
+	static uint bytes_written;
+	static int last_pause;
+	static int volume;
+	static WAVEFORMATEX sound_format;
+	static void Config(HWND hwndParent);
+	static void About(HWND hwndParent);
+	static void Init();
+	static void Quit();
+	static int Open(int samplerate, int numchannels, int bitspersamp, int bufferlenms, int prebufferms);
+	static void Close();
+	static int Write(char* buf, int len);
+	static int CanWrite();
+	static int IsPlaying();
+	static int Pause(int pause);
+	static void SetVolume(int volume);
+	static void SetPan(int pan);
+	static void Flush(int t);
+	static int GetOutputTime();
+	static int GetWrittenTime();
+public:
+	CustomOutPlugin();
+	~CustomOutPlugin();
+	inline WinampOutModule* getModule() {
+		return &mod;
+	}
+	// This method is not part of the winamp plugin
+	static void setTempo(int tempo);
 };
 
 class WinampInPlugin : public WinampPlugin {
 private:
 	WinampInModule* mod;
-	WinampOutPlugin* outPlugin;
+	WinampOutModule* outMod;
 	inline LPCSTR procName() const {
 		return "winampGetInModule2";
 	}
 	bool openModule(FARPROC procAddress);
 	void closeModule();
 public:
-	WinampInPlugin(WinampOutPlugin* outPlugin);
+	WinampInPlugin(WinampOutModule* outMod);
 	~WinampInPlugin();
 
 	int play(const char* fn);
@@ -64,4 +101,6 @@ public:
 	// volume stuff
 	void setVolume(int volume);	// from 0 to 255.. usually just call outMod->SetVolume
 	void setPan(int pan);	// from -127 to 127.. usually just call outMod->SetPan
+	// This method is not part of the winamp plugin
+	void setTempo(int tempo);
 };
