3bcfb8c5208a57852613eb33dff1f10612d4be58
myst6re <myst6re@gmail.com>
2020-04-28 15:07:41 +0200

    Adding transition thread

--- src/winamp/music.h
+++ src/winamp/music.h
@@ -11,12 +11,12 @@
 void winamp_music_init();
 void winamp_play_music(char* midi, uint id);
 void winamp_stop_music();
-void winamp_cross_fade_music(char* midi, uint id, uint time);
+void winamp_cross_fade_music(char* midi, uint id, int time);
 void winamp_pause_music();
 void winamp_resume_music();
 bool winamp_music_status();
-void winamp_set_master_music_volume(uint volume);
-void winamp_set_music_volume(uint volume);
-void winamp_set_music_volume_trans(uint volume, uint step);
+void winamp_set_master_music_volume(int volume);
+void winamp_set_music_volume(int volume);
+void winamp_set_music_volume_trans(int volume, int step);
 void winamp_set_music_tempo(unsigned char tempo);
 void winamp_music_cleanup();
