033d4f27570f1d0990a56c66cb0da6ea46524acb
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2020-05-04 19:40:34 +0200

    Add specification source

--- src/hext.h
+++ src/hext.h
@@ -1,3 +1,12 @@
+/*
+	HEXT Specification by DLPB
+
+	See https://forums.qhimm.com/index.php?topic=13574.0
+
+	This implementation may differ from the original specification as new required functionalities will be implemented,
+	in order to ease modders life.
+*/
+
 #pragma once
 
 #include <algorithm>
