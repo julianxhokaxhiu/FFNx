72a774902c15203cd943939b8351585937564d1c
myst6re <myst6re@gmail.com>
2020-05-12 03:40:35 +0200

    Fast resuming out plugin too

--- src/winamp/winamp_ext.h
+++ src/winamp/winamp_ext.h
@@ -1,10 +1,19 @@
 #pragma once
 
 #define IN_CONTEXT_VER 0x1
+#define OUT_CONTEXT_VER 0x1
+
+struct WinampOutContext {
+	int version;
+	void (*Duplicate)();
+	int (*Resume)(int samplerate, int numchannels, int bitspersamp, int bufferlenms, int prebufferms);
+	int (*CancelDuplicate)();
+};
 
 struct WinampInContext {
 	int version;
 	void (*Duplicate)();
 	int (*Resume)(const char* fn);
-	bool (*CancelDuplicate)();
+	int (*CancelDuplicate)();
+	WinampOutContext* outContext;
 };
