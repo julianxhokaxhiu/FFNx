5686a8859491ac19e04ccab23b9b2ff6b1e6e33b
Chris Rizzitello <rizzitello@kde.org>
2020-05-15 19:30:04 -0400

    GPLv3 Compliance

--- src/winamp/music.h
+++ src/winamp/music.h
@@ -1,3 +1,23 @@
+/****************************************************************************/
+//    Copyright (C) 2009 Aali132                                            //
+//    Copyright (C) 2018 quantumpencil                                      //
+//    Copyright (C) 2018 Maxime Bacoux                                      //
+//    Copyright (C) 2020 Julian Xhokaxhiu                                   //
+//    Copyright (C) 2020 myst6re                                            //
+//    Copyright (C) 2020 Chris Rizzitello                                   //
+//                                                                          //
+//    This file is part of FFNx                                             //
+//                                                                          //
+//    FFNx is free software: you can redistribute it and/or modify          //
+//    it under the terms of the GNU General Public License as published by  //
+//    the Free Software Foundation, either version 3 of the License         //
+//                                                                          //
+//    FFNx is distributed in the hope that it will be useful,               //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//    GNU General Public License for more details.                          //
+/****************************************************************************/
+
 #pragma once
 
 #include <windows.h>
@@ -27,4 +47,3 @@ void winamp_set_music_volume_trans(int volume, int frames);
 void winamp_set_music_tempo(unsigned char tempo);
 void winamp_remember_playing_time();
 void winamp_music_cleanup();
-
