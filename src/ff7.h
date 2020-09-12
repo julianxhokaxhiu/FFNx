5ee577a4a59862b5b92fcdfb8cd1bb481a4ea9aa
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2020-09-13 00:52:57 +0200

    FF7: Add ASK voice acting

--- src/ff7.h
+++ src/ff7.h
@@ -1642,6 +1642,7 @@ struct ff7_externals
 	uint32_t opcode_akao2;
 	uint32_t opcode_gameover;
 	uint32_t opcode_message;
+	uint32_t opcode_ask;
 	uint32_t sfx_play_summon;
 	uint32_t sfx_fill_buffer_from_audio_dat;
 	uint32_t battle_summon_leviathan_loop;
@@ -1670,6 +1671,8 @@ struct ff7_externals
 	WORD* word_CC1638;
 	uint32_t sub_630D50;
 	WORD* opcode_message_loop_code;
+	uint32_t sub_6310A1;
+	WORD* opcode_ask_question_code;
 };
 
 uint32_t ff7gl_load_group(uint32_t group_num, struct matrix_set *matrix_set, struct p_hundred *hundred_data, struct p_group *group_data, struct polygon_data *polygon_data, struct ff7_polygon_set *polygon_set, struct ff7_game_obj *game_object);
--- src/voice.cpp
+++ src/voice.cpp
@@ -26,6 +26,7 @@
 #include "patch.h"
 
 int (*opcode_old_message)();
+int (*opcode_old_ask)(int);
 
 void play_voice(char* field_name, byte dialog_id, byte page_count)
 {
@@ -37,6 +38,15 @@ void play_voice(char* field_name, byte dialog_id, byte page_count)
 
 	if (!nxAudioEngine.canPlayVoice(name) && page_count == 0)
 		sprintf(name, "%s/%u", field_name, dialog_id);
+
+	nxAudioEngine.playVoice(name);
+}
+
+void play_option(char* field_name, byte dialog_id, byte option_count)
+{
+	char name[MAX_PATH];
+
+	sprintf(name, "%s/%u_%u", field_name, dialog_id, option_count);
 
 	nxAudioEngine.playVoice(name);
 }
@@ -66,7 +76,7 @@ int opcode_voice_message()
 	if (is_new_dialog || is_page_changing)
 	{
 		play_voice(field_name, dialog_id, message_page_count);
-		if (trace_all || trace_opcodes) trace("opcode[%s]: field=%s,window_id=%u,dialog_id=%u,paging_id=%u\n", __func__, field_name, window_id, dialog_id, message_page_count);
+		if (trace_all || trace_opcodes) trace("opcode[MESSAGE]: field=%s,window_id=%u,dialog_id=%u,paging_id=%u\n", field_name, window_id, dialog_id, message_page_count);
 	}
 	else if (is_dialog_closing)
 	{
@@ -78,11 +88,55 @@ int opcode_voice_message()
 	return opcode_old_message();
 }
 
+int opcode_voice_ask(int unk)
+{
+	static byte message_page_count = 0;
+	static WORD message_last_opcode = 0;
+	static WORD message_last_option = 0;
+
+	byte window_id = get_field_parameter(1);
+	byte dialog_id = get_field_parameter(2);
+	byte message_current_opcode = ff7_externals.opcode_message_loop_code[24 * window_id];
+	byte message_current_option = (ff7_externals.opcode_ask_question_code[24 * window_id] - 6) / 16;
+	char* field_name = strrchr(ff7_externals.field_file_name, 92) + 1;
+
+	bool is_new_dialog = (message_current_opcode == 0);
+	bool is_page_changing = (message_last_opcode == 14 && message_current_opcode == 2);
+	bool is_dialog_closing = (message_last_opcode != message_current_opcode && message_current_opcode == 7);
+	bool is_dialog_closed = (message_last_opcode == 7 && message_current_opcode == 0);
+	bool is_dialog_option_changed = (message_last_option != message_current_option);
+
+	if (is_new_dialog) message_page_count = 0;
+	if (is_page_changing) message_page_count++;
+	if (is_new_dialog || is_page_changing)
+	{
+		play_voice(field_name, dialog_id, message_page_count);
+		if (trace_all || trace_opcodes) trace("opcode[ASK]: field=%s,window_id=%u,dialog_id=%u,paging_id=%u\n", field_name, window_id, dialog_id, message_page_count);
+	}
+	else if (is_dialog_option_changed)
+	{
+		play_option(field_name, dialog_id, message_current_option);
+		if (trace_all || trace_opcodes) trace("opcode[ASK]: field=%s,window_id=%u,dialog_id=%u,option_id=%u\n", field_name, window_id, dialog_id, message_current_option);
+	}
+	else if (is_dialog_closing)
+	{
+		stop_voice();
+	}
+
+	message_last_option = message_current_option;
+	message_last_opcode = message_current_opcode;
+
+	return opcode_old_ask(unk);
+}
+
 void voice_init()
 {
 	if (!ff8)
 	{
 		opcode_old_message = (int (*)())ff7_externals.opcode_message;
 		patch_code_dword((uint32_t)&common_externals.execute_opcode_table[0x40], (DWORD)&opcode_voice_message);
+
+		opcode_old_ask = (int (*)(int))ff7_externals.opcode_ask;
+		patch_code_dword((uint32_t)&common_externals.execute_opcode_table[0x48], (DWORD)&opcode_voice_ask);
 	}
 }
