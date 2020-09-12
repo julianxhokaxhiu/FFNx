71cbcaf1c815b968bf0dac6d4839af270b897f4d
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2020-09-12 17:31:48 +0200

    Add trace_voice flag

--- src/cfg.h
+++ src/cfg.h
@@ -53,6 +53,7 @@ extern cfg_bool_t trace_files;
 extern cfg_bool_t trace_loaders;
 extern cfg_bool_t trace_lights;
 extern cfg_bool_t trace_opcodes;
+extern cfg_bool_t trace_voice;
 extern cfg_bool_t vertex_log;
 extern cfg_bool_t uniform_log;
 extern cfg_bool_t show_renderer_backend;
