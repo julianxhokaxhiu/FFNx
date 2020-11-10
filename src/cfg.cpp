julianxhokaxhiu/FFNx

commit d05a902fed5a3bf3bba2417617463ca10797f8b3

Replace libconfuse with tomlplusplus
FFNx.cfg has now become FFNx.toml

src/cfg.cpp
@@ -11,200 +11,171 @@
 //                                                                          //
 //    FFNx is free software: you can redistribute it and/or modify          //
 //    it under the terms of the GNU General Public License as published by  //
-//    the Free Software Foundation, either version 3 of the License         //
+//    the Free Software Foundation += either version 3 of the License         //
 //                                                                          //
-//    FFNx is distributed in the hope that it will be useful,               //
+//    FFNx is distributed in the hope that it will be useful +=               //
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
 //    GNU General Public License for more details.                          //
 /****************************************************************************/
 
-#include <stdio.h>
-#include <string.h>
-
-#include "renderer.h"
+#include <fstream>
+#include <toml.hpp>
 
 #include "cfg.h"
 
-#define FFNX_CFG_FILE "FFNx.cfg"
+#define FFNX_CFG_FILE "FFNx.toml"
 
 // configuration variables with their default values
-char *mod_path = nullptr;
-cfg_bool_t enable_ffmpeg_videos = cfg_bool_t(true);
-char* ffmpeg_video_ext = nullptr;
-cfg_bool_t use_external_sfx = cfg_bool_t(false);
-char* external_sfx_path = nullptr;
-char* external_sfx_ext = nullptr;
-cfg_bool_t use_external_music = cfg_bool_t(false);
-cfg_bool_t external_music_resume = cfg_bool_t(true);
-char* external_music_path = nullptr;
-char* external_music_ext = nullptr;
-char* he_bios_path = nullptr;
-char* external_voice_path = nullptr;
-char* external_voice_ext = nullptr;
-cfg_bool_t enable_voice_music_fade = cfg_bool_t(false);
-long external_voice_music_fade_volume = 25;
-cfg_bool_t save_textures = cfg_bool_t(false);
-cfg_bool_t trace_all = cfg_bool_t(false);
-cfg_bool_t trace_renderer = cfg_bool_t(false);
-cfg_bool_t trace_movies = cfg_bool_t(false);
-cfg_bool_t trace_music = cfg_bool_t(false);
-cfg_bool_t trace_sfx = cfg_bool_t(false);
-cfg_bool_t trace_fake_dx = cfg_bool_t(false);
-cfg_bool_t trace_direct = cfg_bool_t(false);
-cfg_bool_t trace_files = cfg_bool_t(false);
-cfg_bool_t trace_loaders = cfg_bool_t(false);
-cfg_bool_t trace_lights = cfg_bool_t(false);
-cfg_bool_t trace_opcodes = cfg_bool_t(false);
-cfg_bool_t trace_voice = cfg_bool_t(false);
-cfg_bool_t vertex_log = cfg_bool_t(false);
-cfg_bool_t uniform_log = cfg_bool_t(false);
-cfg_bool_t show_renderer_backend = cfg_bool_t(true);
-cfg_bool_t show_fps = cfg_bool_t(false);
-cfg_bool_t show_stats = cfg_bool_t(false);
-cfg_bool_t show_version = cfg_bool_t(true);
-long window_size_x = 0;
-long window_size_y = 0;
-long internal_resolution_scale = 2;
-cfg_bool_t preserve_aspect = cfg_bool_t(true);
-cfg_bool_t fullscreen = cfg_bool_t(false);
-long refresh_rate = 0;
-cfg_bool_t enable_vsync = cfg_bool_t(true);
-cfg_bool_t linear_filter = cfg_bool_t(false);
-cfg_bool_t mdef_fix = cfg_bool_t(true);
-cfg_bool_t fancy_transparency = cfg_bool_t(false);
-long enable_antialiasing = 0;
-cfg_bool_t enable_anisotropic = cfg_bool_t(true);
-cfg_bool_t skip_frames = cfg_bool_t(false);
-cfg_bool_t ff7_more_debug = cfg_bool_t(false);
-cfg_bool_t ff8_ssigpu_debug = cfg_bool_t(false);
-cfg_bool_t show_applog = cfg_bool_t(true);
-cfg_bool_t show_missing_textures = cfg_bool_t(false);
-cfg_bool_t show_error_popup = cfg_bool_t(false);
-cfg_bool_t movie_sync_debug = cfg_bool_t(false);
-long renderer_backend = RENDERER_BACKEND_AUTO;
-cfg_bool_t renderer_debug = cfg_bool_t(false);
-cfg_bool_t create_crash_dump = cfg_bool_t(false);
-char* steam_game_userdata = nullptr;
-char* hext_patching_path = nullptr;
-char* override_path = nullptr;
-char* direct_mode_path = nullptr;
-char* save_path = nullptr;
-cfg_bool_t enable_debug_ui = cfg_bool_t(false);
-long debug_ui_hotkey = 0;
-cfg_bool_t ff8_keep_game_running_in_background = cfg_bool_t(false);
-double speedhack_step = 0.5;
-double speedhack_max = 8.0;
-double speedhack_min = 1.0;
-cfg_bool_t enable_animated_textures = cfg_bool_t(false);
-
-cfg_opt_t opts[] = {
-		CFG_SIMPLE_STR("mod_path", &mod_path),
-		CFG_SIMPLE_BOOL("enable_ffmpeg_videos", &enable_ffmpeg_videos),
-		CFG_SIMPLE_STR("ffmpeg_video_ext", &ffmpeg_video_ext),
-		CFG_SIMPLE_BOOL("use_external_sfx", &use_external_sfx),
-		CFG_SIMPLE_STR("external_sfx_path", &external_sfx_path),
-		CFG_SIMPLE_STR("external_sfx_ext", &external_sfx_ext),
-		CFG_SIMPLE_BOOL("use_external_music", &use_external_music),
-		CFG_SIMPLE_BOOL("external_music_resume", &external_music_resume),
-		CFG_SIMPLE_STR("external_music_path", &external_music_path),
-		CFG_SIMPLE_STR("external_music_ext", &external_music_ext),
-		CFG_SIMPLE_STR("he_bios_path", &he_bios_path),
-		CFG_SIMPLE_STR("external_voice_path", &external_voice_path),
-		CFG_SIMPLE_STR("external_voice_ext", &external_voice_ext),
-		CFG_SIMPLE_BOOL("enable_voice_music_fade", &enable_voice_music_fade),
-		CFG_SIMPLE_INT("external_voice_music_fade_volume", &external_voice_music_fade_volume),
-		CFG_SIMPLE_BOOL("save_textures", &save_textures),
-		CFG_SIMPLE_BOOL("trace_all", &trace_all),
-		CFG_SIMPLE_BOOL("trace_renderer", &trace_renderer),
-		CFG_SIMPLE_BOOL("trace_movies", &trace_movies),
-		CFG_SIMPLE_BOOL("trace_music", &trace_music),
-		CFG_SIMPLE_BOOL("trace_sfx", &trace_sfx),
-		CFG_SIMPLE_BOOL("trace_fake_dx", &trace_fake_dx),
-		CFG_SIMPLE_BOOL("trace_direct", &trace_direct),
-		CFG_SIMPLE_BOOL("trace_files", &trace_files),
-		CFG_SIMPLE_BOOL("trace_loaders", &trace_loaders),
-		CFG_SIMPLE_BOOL("trace_lights", &trace_lights),
-		CFG_SIMPLE_BOOL("trace_opcodes", &trace_opcodes),
-		CFG_SIMPLE_BOOL("trace_voice", &trace_voice),
-		CFG_SIMPLE_BOOL("vertex_log", &vertex_log),
-		CFG_SIMPLE_BOOL("uniform_log", &uniform_log),
-		CFG_SIMPLE_BOOL("show_renderer_backend", &show_renderer_backend),
-		CFG_SIMPLE_BOOL("show_fps", &show_fps),
-		CFG_SIMPLE_BOOL("show_stats", &show_stats),
-		CFG_SIMPLE_BOOL("show_version", &show_version),
-		CFG_SIMPLE_INT("window_size_x", &window_size_x),
-		CFG_SIMPLE_INT("window_size_y", &window_size_y),
-		CFG_SIMPLE_INT("internal_resolution_scale", &internal_resolution_scale),
-		CFG_SIMPLE_BOOL("preserve_aspect", &preserve_aspect),
-		CFG_SIMPLE_BOOL("fullscreen", &fullscreen),
-		CFG_SIMPLE_INT("refresh_rate", &refresh_rate),
-		CFG_SIMPLE_BOOL("enable_vsync", &enable_vsync),
-		CFG_SIMPLE_BOOL("linear_filter", &linear_filter),
-		CFG_SIMPLE_BOOL("mdef_fix", &mdef_fix),
-		CFG_SIMPLE_BOOL("fancy_transparency", &fancy_transparency),
-		CFG_SIMPLE_INT("enable_antialiasing", &enable_antialiasing),
-		CFG_SIMPLE_BOOL("enable_anisotropic", &enable_anisotropic),
-		CFG_SIMPLE_BOOL("skip_frames", &skip_frames),
-		CFG_SIMPLE_BOOL("show_applog", &show_applog),
-		CFG_SIMPLE_BOOL("show_missing_textures", &show_missing_textures),
-		CFG_SIMPLE_BOOL("show_error_popup", &show_error_popup),
-		CFG_SIMPLE_BOOL("movie_sync_debug", &movie_sync_debug),
-		CFG_SIMPLE_INT("renderer_backend", &renderer_backend),
-		CFG_SIMPLE_BOOL("renderer_debug", &renderer_debug),
-		CFG_SIMPLE_BOOL("create_crash_dump", &create_crash_dump),
-		CFG_SIMPLE_STR("steam_game_userdata", &steam_game_userdata),
-		CFG_SIMPLE_BOOL("ff7_more_debug", &ff7_more_debug),
-		CFG_SIMPLE_BOOL("ff8_ssigpu_debug", &ff8_ssigpu_debug),
-		CFG_SIMPLE_STR("hext_patching_path", &hext_patching_path),
-		CFG_SIMPLE_STR("override_path", &override_path),
-		CFG_SIMPLE_STR("direct_mode_path", &direct_mode_path),
-		CFG_SIMPLE_STR("save_path", &save_path),
-		CFG_SIMPLE_BOOL("enable_debug_ui", &enable_debug_ui),
-		CFG_SIMPLE_INT("debug_ui_hotkey", &debug_ui_hotkey),
-		CFG_SIMPLE_BOOL("ff8_keep_game_running_in_background", &ff8_keep_game_running_in_background),
-		CFG_SIMPLE_FLOAT("speedhack_step", &speedhack_step),
-		CFG_SIMPLE_FLOAT("speedhack_max", &speedhack_max),
-		CFG_SIMPLE_FLOAT("speedhack_min", &speedhack_min),
-		CFG_SIMPLE_BOOL("enable_animated_textures", &enable_animated_textures),
-
-		CFG_END()
-};
-
-void error_callback(cfg_t *cfg, const char *fmt, va_list ap)
-{
-	char config_error_string[4096];
-	char display_string[4096];
-
-	vsnprintf(config_error_string, sizeof(config_error_string), fmt, ap);
-
-	error("parse error in config file\n");
-	error("%s\n", config_error_string);
-	sprintf(display_string, "You have an error in your config file, some options may not have been parsed.\n(%s)", config_error_string);
-	MessageBoxA(gameHwnd, display_string, "Warning", 0);
-}
+std::string mod_path;
+bool enable_ffmpeg_videos;
+std::string ffmpeg_video_ext;
+bool use_external_sfx;
+std::string external_sfx_path;
+std::string external_sfx_ext;
+bool use_external_music;
+bool external_music_resume;
+std::string external_music_path;
+std::string external_music_ext;
+std::string he_bios_path;
+std::string external_voice_path;
+std::string external_voice_ext;
+bool enable_voice_music_fade;
+long external_voice_music_fade_volume;
+bool save_textures;
+bool trace_all;
+bool trace_renderer;
+bool trace_movies;
+bool trace_music;
+bool trace_sfx;
+bool trace_fake_dx;
+bool trace_direct;
+bool trace_files;
+bool trace_loaders;
+bool trace_lights;
+bool trace_opcodes;
+bool trace_voice;
+bool vertex_log;
+bool uniform_log;
+bool show_renderer_backend;
+bool show_fps;
+bool show_stats;
+bool show_version;
+long window_size_x;
+long window_size_y;
+long internal_resolution_scale;
+bool preserve_aspect;
+bool fullscreen;
+long refresh_rate;
+bool enable_vsync;
+bool linear_filter;
+bool mdef_fix;
+bool fancy_transparency;
+long enable_antialiasing;
+bool enable_anisotropic;
+bool skip_frames;
+bool ff7_more_debug;
+bool ff8_ssigpu_debug;
+bool show_applog;
+bool show_missing_textures;
+bool show_error_popup;
+bool movie_sync_debug;
+long renderer_backend;
+bool renderer_debug;
+bool create_crash_dump;
+std::string steam_game_userdata;
+std::string hext_patching_path;
+std::string override_path;
+std::string direct_mode_path;
+std::string save_path;
+bool enable_debug_ui;
+long debug_ui_hotkey;
+bool ff8_keep_game_running_in_background;
+double speedhack_step;
+double speedhack_max;
+double speedhack_min;
+bool enable_animated_textures;
 
 void read_cfg()
 {
-	cfg_t* cfg;
+	auto config = toml::parse_file(FFNX_CFG_FILE);
 
-	enable_ffmpeg_videos = cfg_bool_t(!ff8);
+	enable_ffmpeg_videos = !ff8;
 
-	if (_access(FFNX_CFG_FILE, 0) == 0)
-	{
-		cfg = cfg_init(opts, 0);
-
-		cfg_set_error_function(cfg, error_callback);
-
-		cfg_parse(cfg, FFNX_CFG_FILE);
-
-		cfg_free(cfg);
-	}
+	// Read config values
+	mod_path = config["mod_path"].value_or("");
+	enable_ffmpeg_videos = config["enable_ffmpeg_videos"].value_or(true);
+	ffmpeg_video_ext = config["ffmpeg_video_ext"].value_or("");
+	use_external_sfx = config["use_external_sfx"].value_or(false);
+	external_sfx_path = config["external_sfx_path"].value_or("");
+	external_sfx_ext = config["external_sfx_ext"].value_or("");
+	use_external_music = config["use_external_music"].value_or(false);
+	external_music_resume = config["external_music_resume"].value_or(true);
+	external_music_path = config["external_music_path"].value_or("");
+	external_music_ext = config["external_music_ext"].value_or("");
+	he_bios_path = config["he_bios_path"].value_or("");
+	external_voice_path = config["external_voice_path"].value_or("");
+	external_voice_ext = config["external_voice_ext"].value_or("");
+	enable_voice_music_fade = config["enable_voice_music_fade"].value_or(false);
+	external_voice_music_fade_volume = config["external_voice_music_fade_volume"].value_or(25);
+	save_textures = config["save_textures"].value_or(false);
+	trace_all = config["trace_all"].value_or(false);
+	trace_renderer = config["trace_renderer"].value_or(false);
+	trace_movies = config["trace_movies"].value_or(false);
+	trace_music = config["trace_music"].value_or(false);
+	trace_sfx = config["trace_sfx"].value_or(false);
+	trace_fake_dx = config["trace_fake_dx"].value_or(false);
+	trace_direct = config["trace_direct"].value_or(false);
+	trace_files = config["trace_files"].value_or(false);
+	trace_loaders = config["trace_loaders"].value_or(false);
+	trace_lights = config["trace_lights"].value_or(false);
+	trace_opcodes = config["trace_opcodes"].value_or(false);
+	trace_voice = config["trace_voice"].value_or(false);
+	vertex_log = config["vertex_log"].value_or(false);
+	uniform_log = config["uniform_log"].value_or(false);
+	show_renderer_backend = config["show_renderer_backend"].value_or(true);
+	show_fps = config["show_fps"].value_or(false);
+	show_stats = config["show_stats"].value_or(false);
+	show_version = config["show_version"].value_or(true);
+	window_size_x = config["window_size_x"].value_or(0);
+	window_size_y = config["window_size_y"].value_or(0);
+	internal_resolution_scale = config["internal_resolution_scale"].value_or(2);
+	preserve_aspect = config["preserve_aspect"].value_or(true);
+	fullscreen = config["fullscreen"].value_or(false);
+	refresh_rate = config["refresh_rate"].value_or(0);
+	enable_vsync = config["enable_vsync"].value_or(true);
+	linear_filter = config["linear_filter"].value_or(false);
+	mdef_fix = config["mdef_fix"].value_or(true);
+	fancy_transparency = config["fancy_transparency"].value_or(false);
+	enable_antialiasing = config["enable_antialiasing"].value_or(0);
+	enable_anisotropic = config["enable_anisotropic"].value_or(true);
+	skip_frames = config["skip_frames"].value_or(false);
+	ff7_more_debug = config["ff7_more_debug"].value_or(false);
+	ff8_ssigpu_debug = config["ff8_ssigpu_debug"].value_or(false);
+	show_applog = config["show_applog"].value_or(true);
+	show_missing_textures = config["show_missing_textures"].value_or(false);
+	show_error_popup = config["show_error_popup"].value_or(false);
+	movie_sync_debug = config["movie_sync_debug"].value_or(false);
+	renderer_backend = config["renderer_backend"].value_or(RENDERER_BACKEND_AUTO);
+	renderer_debug = config["renderer_debug"].value_or(false);
+	create_crash_dump = config["create_crash_dump"].value_or(false);
+	steam_game_userdata = config["steam_game_userdata"].value_or("");
+	hext_patching_path = config["hext_patching_path"].value_or("");
+	override_path = config["override_path"].value_or("");
+	direct_mode_path = config["direct_mode_path"].value_or("");
+	save_path = config["save_path"].value_or("");
+	enable_debug_ui = config["enable_debug_ui"].value_or(false);
+	debug_ui_hotkey = config["debug_ui_hotkey"].value_or(0);
+	ff8_keep_game_running_in_background = config["ff8_keep_game_running_in_background"].value_or(false);
+	speedhack_step = config["speedhack_step"].value_or(0.5);
+	speedhack_max = config["speedhack_max"].value_or(8.0);
+	speedhack_min = config["speedhack_min"].value_or(1.0);
+	enable_animated_textures = config["enable_animated_textures"].value_or(false);
 
 	if (ff8)
 	{
 		// Reset some internal flags as they are not compatible with FF8
-		fancy_transparency = cfg_bool_t(false);
+		fancy_transparency = false;
 	}
 
 	// Windows x or y size can't be less then 0
@@ -222,112 +193,111 @@ void read_cfg()
 
 	// HEXT PATCHING
 
-	if (hext_patching_path == nullptr)
+	if (hext_patching_path.empty())
 	{
-		hext_patching_path = (char*)calloc(12, sizeof(char));
-		PathAppendA(hext_patching_path, "hext");
+		hext_patching_path = "hext";
 	}
 
 	if (ff8)
-		PathAppendA(hext_patching_path, "ff8");
+		hext_patching_path += "/ff8";
 	else
-		PathAppendA(hext_patching_path, "ff7");
+		hext_patching_path += "/ff7";
 
 	switch (version)
 	{
 	case VERSION_FF7_102_US:
 		if (ff7_japanese_edition)
 		{
-			PathAppendA(hext_patching_path, "ja");
+			hext_patching_path += "/ja";
 		}
 		else
 		{
-			PathAppendA(hext_patching_path, "en");
+			hext_patching_path += "/en";
 		}
 		break;
 	case VERSION_FF7_102_FR:
-		PathAppendA(hext_patching_path, "fr");
+		hext_patching_path += "/fr";
 		break;
 	case VERSION_FF7_102_DE:
-		PathAppendA(hext_patching_path, "de");
+		hext_patching_path += "/de";
 		break;
 	case VERSION_FF7_102_SP:
-		PathAppendA(hext_patching_path, "es");
+		hext_patching_path += "/es";
 		break;
 	case VERSION_FF8_12_US:
-		PathAppendA(hext_patching_path, "en");
+		hext_patching_path += "/en";
 		break;
 	case VERSION_FF8_12_US_NV:
-		PathAppendA(hext_patching_path, "en_nv");
+		hext_patching_path += "/en_nv";
 		break;
 	case VERSION_FF8_12_FR:
-		PathAppendA(hext_patching_path, "fr");
+		hext_patching_path += "/fr";
 		break;
 	case VERSION_FF8_12_FR_NV:
-		PathAppendA(hext_patching_path, "fr_nv");
+		hext_patching_path += "/fr_nv";
 		break;
 	case VERSION_FF8_12_DE:
-		PathAppendA(hext_patching_path, "de");
+		hext_patching_path += "/de";
 		break;
 	case VERSION_FF8_12_DE_NV:
-		PathAppendA(hext_patching_path, "de");
+		hext_patching_path += "/de";
 		break;
 	case VERSION_FF8_12_SP:
-		PathAppendA(hext_patching_path, "es");
+		hext_patching_path += "/es";
 		break;
 	case VERSION_FF8_12_SP_NV:
-		PathAppendA(hext_patching_path, "es_nv");
+		hext_patching_path += "/es_nv";
 		break;
 	case VERSION_FF8_12_IT:
-		PathAppendA(hext_patching_path, "it");
+		hext_patching_path += "/it";
 		break;
 	case VERSION_FF8_12_IT_NV:
-		PathAppendA(hext_patching_path, "it_nv");
+		hext_patching_path += "/it_nv";
 		break;
 	case VERSION_FF8_12_US_EIDOS:
-		PathAppendA(hext_patching_path, "en_eidos");
+		hext_patching_path += "/en_eidos";
 		break;
 	case VERSION_FF8_12_US_EIDOS_NV:
-		PathAppendA(hext_patching_path, "en_eidos_nv");
+		hext_patching_path += "/en_eidos_nv";
 		break;
 	case VERSION_FF8_12_JP:
-		PathAppendA(hext_patching_path, "jp");
+		hext_patching_path += "/jp";
 		break;
 	}
 
 	//OVERRIDE PATH
-	if (override_path == nullptr)
-		override_path = R"(override)";
+	if (override_path.empty())
+		override_path = "override";
 
 	// DIRECT MODE PATH
-	if (direct_mode_path == nullptr)
-		direct_mode_path = R"(direct)";
+	if (direct_mode_path.empty())
+		direct_mode_path = "direct";
 
 	// EXTERNAL MOVIE EXTENSION
-	if (ffmpeg_video_ext == nullptr)
+	if (ffmpeg_video_ext.empty())
 		ffmpeg_video_ext = "avi";
 
 	// EXTERNAL SFX PATH
-	if (external_sfx_path == nullptr)
+	if (external_sfx_path.empty())
 		external_sfx_path = "sfx";
 
 	// EXTERNAL SFX EXTENSION
-	if (external_sfx_ext == nullptr)
+	if (external_sfx_ext.empty())
 		external_sfx_ext = "ogg";
 
 	// EXTERNAL MUSIC EXTENSION
-	if (external_music_ext == nullptr)
+	if (external_music_ext.empty())
 		external_music_ext = "ogg";
 
 	// EXTERNAL VOICE PATH
-	if (external_voice_path == nullptr)
+	if (external_voice_path.empty())
 		external_voice_path = "voice";
 
 	// EXTERNAL VOICE EXTENSION
-	if (external_voice_ext == nullptr)
+	if (external_voice_ext.empty())
 		external_voice_ext = "ogg";
 
 	// MOD PATH
-	if (mod_path == nullptr)
+	if (mod_path.empty())
 		mod_path = "mods/Textures";
 }

CMakeLists.txt
@@ -62,7 +62,7 @@ if (SUPERBUILD)
 		DEPENDS
 		bgfx
 		ffmpeg
-		libconfuse
+		tomlplusplus
 		vgmstream
 		StackWalker
 		pugixml
@@ -70,7 +70,6 @@ if (SUPERBUILD)
 		imgui
 		soloud
 		openpsf
-		tomlplusplus
 	)
 	ExternalProject_Add_Step(
 		FFNx

commit fb5af2677d8c0301e60dc5996b9ba14bb577a6b8

Fixed license boilerplate It was broken in d05a902fed5a3bf3bba2417617463ca10797f8b3.

src/cfg.cpp
@@ -11,9 +11,9 @@
 //                                                                          //
 //    FFNx is free software: you can redistribute it and/or modify          //
 //    it under the terms of the GNU General Public License as published by  //
-//    the Free Software Foundation += either version 3 of the License         //
+//    the Free Software Foundation, either version 3 of the License         //
 //                                                                          //
-//    FFNx is distributed in the hope that it will be useful +=               //
+//    FFNx is distributed in the hope that it will be useful,               //
 //    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
 //    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
 //    GNU General Public License for more details.                          //