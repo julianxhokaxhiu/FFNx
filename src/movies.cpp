291beb8dd7de7a13a701b2fca8090bb472e5814c
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2021-01-12 23:35:58 +0100

    Movie: Cleanup

--- src/movies.cpp
+++ src/movies.cpp
@@ -26,30 +26,6 @@
 #include "ff7/defs.h"
 #include "video/movies.h"
 
-void movie_init()
-{
-	if(!ff8)
-	{
-		replace_function(common_externals.prepare_movie, ff7_prepare_movie);
-		replace_function(common_externals.release_movie_objects, ff7_release_movie_objects);
-		replace_function(common_externals.start_movie, ff7_start_movie);
-		replace_function(common_externals.update_movie_sample, ff7_update_movie_sample);
-		replace_function(common_externals.stop_movie, ff7_stop_movie);
-		replace_function(common_externals.get_movie_frame, ff7_get_movie_frame);
-	}
-	else
-	{
-		replace_function(common_externals.prepare_movie, ff8_prepare_movie);
-		replace_function(common_externals.release_movie_objects, ff8_release_movie_objects);
-		replace_function(common_externals.start_movie, ff8_start_movie);
-		replace_function(common_externals.update_movie_sample, ff8_update_movie_sample);
-		replace_function(ff8_externals.draw_movie_frame, draw_current_frame);
-		replace_function(common_externals.stop_movie, ff8_stop_movie);
-	}
-
-	ffmpeg_movie_init();
-}
-
 uint32_t ff7_prepare_movie(char *name, uint32_t loop, struct dddevice **dddevice, uint32_t dd2interface)
 {
 	char drivename[4];
@@ -83,15 +59,31 @@ uint32_t ff7_prepare_movie(char *name, uint32_t loop, struct dddevice **dddevice
 	return true;
 }
 
-void ff7_release_movie_objects()
+uint32_t ff7_update_movie_sample(LPDIRECTDRAWSURFACE surface)
 {
-	if(trace_all || trace_movies) trace("release_movie_objects\n");
+	uint32_t movie_end;
 
-	ff7_stop_movie();
+	ff7_externals.movie_object->movie_end = 0;
 
-	ffmpeg_release_movie_objects();
+	if(!ff7_externals.movie_object->is_playing) return false;
 
-	ff7_externals.movie_object->global_movie_flag = 0;
+retry:
+	movie_end = !ffmpeg_update_movie_sample();
+
+	if(movie_end)
+	{
+		if(trace_all || trace_movies) trace("movie end\n");
+		if(ff7_externals.movie_object->loop)
+		{
+			ffmpeg_loop();
+			goto retry;
+		}
+
+		ff7_externals.movie_object->movie_end = 1;
+		return true;
+	}
+
+	return true;
 }
 
 uint32_t ff7_start_movie()
@@ -120,31 +112,15 @@ uint32_t ff7_stop_movie()
 	return true;
 }
 
-uint32_t ff7_update_movie_sample(LPDIRECTDRAWSURFACE surface)
+void ff7_release_movie_objects()
 {
-	uint32_t movie_end;
+	if(trace_all || trace_movies) trace("release_movie_objects\n");
 
-	ff7_externals.movie_object->movie_end = 0;
+	ff7_stop_movie();
 
-	if(!ff7_externals.movie_object->is_playing) return false;
+	ffmpeg_release_movie_objects();
 
-retry:
-	movie_end = !ffmpeg_update_movie_sample();
-
-	if(movie_end)
-	{
-		if(trace_all || trace_movies) trace("movie end\n");
-		if(ff7_externals.movie_object->loop)
-		{
-			ffmpeg_loop();
-			goto retry;
-		}
-
-		ff7_externals.movie_object->movie_end = 1;
-		return true;
-	}
-
-	return true;
+	ff7_externals.movie_object->global_movie_flag = 0;
 }
 
 void draw_current_frame()
@@ -235,40 +211,6 @@ void ff8_release_movie_objects()
 	ffmpeg_release_movie_objects();
 }
 
-void ff8_start_movie()
-{
-	if(trace_all || trace_movies) trace("start_movie\n");
-
-	if(ff8_externals.movie_object->movie_intro_pak) ff8_externals.movie_object->movie_total_frames = ff8_movie_frames;
-	else
-	{
-		ff8_externals.movie_object->movie_total_frames = ((WORD *)ff8_externals.movie_object->camdata_buffer)[3];
-		trace("%i frames\n", ff8_externals.movie_object->movie_total_frames);
-	}
-
-	ff8_externals.movie_object->field_4C4B0 = 0;
-
-	*ff8_externals.enable_framelimiter = false;
-
-	ff8_update_movie_sample();
-
-	ff8_externals.movie_object->movie_is_playing = true;
-}
-
-void ff8_stop_movie()
-{
-	if(trace_all || trace_movies) trace("stop_movie\n");
-
-	ffmpeg_stop_movie();
-
-	ff8_externals.movie_object->movie_is_playing = false;
-
-	ff8_externals.movie_object->field_4C4AC = 0;
-	ff8_externals.movie_object->field_4C4B0 = 0;
-
-	*ff8_externals.enable_framelimiter = true;
-}
-
 void ff8_update_movie_sample()
 {
 	if(trace_all || trace_movies) trace("update_movie_sample\n");
@@ -311,4 +253,62 @@ void ff8_update_movie_sample()
 	}
 
 	ff8_externals.movie_object->camdata_pointer = &ff8_externals.movie_object->camdata_start[ff8_externals.movie_object->movie_current_frame];
+}
+
+void ff8_start_movie()
+{
+	if(trace_all || trace_movies) trace("start_movie\n");
+
+	if(ff8_externals.movie_object->movie_intro_pak) ff8_externals.movie_object->movie_total_frames = ff8_movie_frames;
+	else
+	{
+		ff8_externals.movie_object->movie_total_frames = ((WORD *)ff8_externals.movie_object->camdata_buffer)[3];
+		trace("%i frames\n", ff8_externals.movie_object->movie_total_frames);
+	}
+
+	ff8_externals.movie_object->field_4C4B0 = 0;
+
+	*ff8_externals.enable_framelimiter = false;
+
+	ff8_update_movie_sample();
+
+	ff8_externals.movie_object->movie_is_playing = true;
+}
+
+void ff8_stop_movie()
+{
+	if(trace_all || trace_movies) trace("stop_movie\n");
+
+	ffmpeg_stop_movie();
+
+	ff8_externals.movie_object->movie_is_playing = false;
+
+	ff8_externals.movie_object->field_4C4AC = 0;
+	ff8_externals.movie_object->field_4C4B0 = 0;
+
+	*ff8_externals.enable_framelimiter = true;
+}
+
+void movie_init()
+{
+	if(!ff8)
+	{
+		replace_function(common_externals.prepare_movie, ff7_prepare_movie);
+		replace_function(common_externals.release_movie_objects, ff7_release_movie_objects);
+		replace_function(common_externals.start_movie, ff7_start_movie);
+		replace_function(common_externals.update_movie_sample, ff7_update_movie_sample);
+		replace_function(common_externals.stop_movie, ff7_stop_movie);
+		replace_function(common_externals.get_movie_frame, ff7_get_movie_frame);
+	}
+	else
+	{
+		replace_function(common_externals.prepare_movie, ff8_prepare_movie);
+		replace_function(common_externals.release_movie_objects, ff8_release_movie_objects);
+		replace_function(common_externals.start_movie, ff8_start_movie);
+		replace_function(common_externals.update_movie_sample, ff8_update_movie_sample);
+		replace_function(ff8_externals.draw_movie_frame, draw_current_frame);
+		replace_function(common_externals.stop_movie, ff8_stop_movie);
+	}
+
+	ffmpeg_movie_init();
 }
--- src/movies.h
+++ src/movies.h
@@ -24,15 +24,6 @@
 #include "common.h"
 
 void movie_init();
-uint32_t ff7_prepare_movie(char *, uint32_t, struct dddevice **, uint32_t);
 void ff7_release_movie_objects();
-uint32_t ff7_start_movie();
-uint32_t ff7_update_movie_sample(LPDIRECTDRAWSURFACE);
-uint32_t ff7_stop_movie();
-uint32_t ff7_get_movie_frame();
-void draw_current_frame();
-void ff8_prepare_movie(uint32_t disc, uint32_t movie);
 void ff8_release_movie_objects();
-void ff8_start_movie();
-void ff8_update_movie_sample();
 void ff8_stop_movie();
