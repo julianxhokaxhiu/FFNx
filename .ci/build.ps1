f8b00bfcc41413be9767219d6f2fcdb4ff2a8df4
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2021-08-08 15:28:25 +0200

    CI: Improve pipeline

--- .ci/build.ps1
+++ .ci/build.ps1
@@ -34,10 +34,16 @@ elseif ($env:_BUILD_BRANCH -like "refs/tags/*")
 $env:_RELEASE_VERSION = "v${env:_BUILD_VERSION}"
 
 $vcpkgRoot = "C:\vcpkg"
+$vcpkgBaseline = [string](jq --arg baseline "builtin-baseline" -r '.[$baseline]' vcpkg.json)
+$vcpkgOriginUrl = &"git" -C $vcpkgRoot remote get-url origin
+$vcpkgBranchName = &"git" -C $vcpkgRoot branch --show-current
 
 Write-Output "--------------------------------------------------"
 Write-Output "BUILD CONFIGURATION: $env:_RELEASE_CONFIGURATION"
 Write-Output "RELEASE VERSION: $env:_RELEASE_VERSION"
+Write-Output "VCPKG ORIGIN: $vcpkgOriginUrl"
+Write-Output "VCPKG BRANCH: $vcpkgBranchName"
+Write-Output "VCPKG BASELINE: $vcpkgBaseline"
 Write-Output "--------------------------------------------------"
 
 Write-Host "##vso[task.setvariable variable=_BUILD_VERSION;]${env:_BUILD_VERSION}"
@@ -54,7 +60,10 @@ Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
   }
 }
 
-git -C $vcpkgRoot pull
+git -C $vcpkgRoot pull --unshallow
+git -C $vcpkgRoot clean -fxd
+
+cmd.exe /c "call $vcpkgRoot\bootstrap-vcpkg.bat"
 
 mkdir "C:\vcpkg\downloads\tools\yasm\1.3.0.6" | Out-Null
 Invoke-WebRequest -Uri "http://www.tortall.net/projects/yasm/snapshots/v1.3.0.6.g1962/yasm-1.3.0.6.g1962.exe" -SkipCertificateCheck -OutFile "C:\vcpkg\downloads\tools\yasm\1.3.0.6\yasm.exe"
