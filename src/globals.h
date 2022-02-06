d41428af5eb62e86964247c2d6d2c8ea057687c0
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2022-02-06 21:32:45 +0100

    Compilation: Use the correct library instead of declaring that ourselves

--- src/globals.h
+++ src/globals.h
@@ -29,8 +29,6 @@
 extern "C" {
 #endif
 
-extern const GUID IID_ICodecAPI = { 0x901db4c7, 0x31ce, 0x41a2, 0x85,0xdc, 0x8f,0xa0,0xbf,0x41,0xb8,0xda };
-
 #include <libavcodec/avcodec.h>
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
