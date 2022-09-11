7c5199bea04d8f584e84df24fb9005b897aeedf6
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2022-09-11 19:41:30 +0200

    CI: Minor improvements
    - Upload artifacts to the job when it's not an official Github release ( Canary/Stable )
    - Optimize a bit some conditions checks: allow manual builds on custom branches to be built and upload artifacts on the job, but not trigger any other task

--- .ci/build.ps1
+++ .ci/build.ps1
@@ -25,11 +25,13 @@ $env:_RELEASE_PATH = ".build"
 if ($env:_BUILD_BRANCH -eq "refs/heads/master" -Or $env:_BUILD_BRANCH -eq "refs/tags/canary")
 {
   $env:_IS_BUILD_CANARY = "true"
+  $env:_IS_GITHUB_RELEASE = "true"
 }
 elseif ($env:_BUILD_BRANCH -like "refs/tags/*")
 {
   $env:_CHANGELOG_VERSION = $env:_BUILD_VERSION.Substring(0,$env:_BUILD_VERSION.LastIndexOf('.')).Replace('.','')
   $env:_BUILD_VERSION = $env:_BUILD_VERSION.Substring(0,$env:_BUILD_VERSION.LastIndexOf('.')) + ".0"
+  $env:_IS_GITHUB_RELEASE = "true"
 }
 $env:_RELEASE_VERSION = "v${env:_BUILD_VERSION}"
 
@@ -49,6 +51,7 @@ Write-Output "--------------------------------------------------"
 Write-Host "##vso[task.setvariable variable=_BUILD_VERSION;]${env:_BUILD_VERSION}"
 Write-Host "##vso[task.setvariable variable=_RELEASE_VERSION;]${env:_RELEASE_VERSION}"
 Write-Host "##vso[task.setvariable variable=_IS_BUILD_CANARY;]${env:_IS_BUILD_CANARY}"
+Write-Host "##vso[task.setvariable variable=_IS_GITHUB_RELEASE;]${env:_IS_GITHUB_RELEASE}"
 Write-Host "##vso[task.setvariable variable=_CHANGELOG_VERSION;]${env:_CHANGELOG_VERSION}"
 
 # Load vcvarsall environment for x86
