0314be8c4a99a8c235384932a1036c05ad9bbbf8
ChthonVII <cwbussard@gmail.com>
2023-02-28 01:04:24 +0900

    More FMV changes: (1) Add support PAL color gamut and gamma function (2) Increase precision in SMPTE170 gamma function (3) Increase precision in SMPTE-C gamut conversion matrix (4) Experiment: replace user-controlled pure gamma curve with 13thMonkey gamma curve

--- misc/FFNx.common.sh
+++ misc/FFNx.common.sh
@@ -24,7 +24,7 @@ vec3 toLinear(vec3 _rgb)
 
 vec3 toLinearSMPTE170M(vec3 _rgb)
 {
-	bvec3 cutoff = lessThan(_rgb.rgb, vec3_splat(0.081));
+	bvec3 cutoff = lessThan(_rgb.rgb, vec3_splat(0.0812));
 	vec3 higher = pow((_rgb.rgb + vec3_splat(0.099)) / vec3_splat(1.099), (vec3_splat(1.0) / vec3_splat(0.45)));
 	vec3 lower = _rgb.rgb / vec3_splat(4.5);
 
@@ -34,6 +34,22 @@ vec3 toLinearSMPTE170M(vec3 _rgb)
 vec3 toLinear2pt2(vec3 _rgb)
 {
 	return pow(_rgb.rgb, vec3_splat(2.2));
+}
+
+// Microsoft says PAL uses a pure 2.8 gamma curve. See: https://learn.microsoft.com/en-us/windows/win32/api/mfobjects/ne-mfobjects-mfvideotransferfunction
+// ffmpeg thinks there *should* be a linear toe slope, but uses a pure curve since they cannot find any documentation for it. See: https://github.com/FFmpeg/FFmpeg/blob/master/libavfilter/vf_colorspace.c#L162
+// In any event Poynton says 2.8 is "unrealistically high" and PAL CRT units did not really behave like that
+// PAL switched to the SMPTE170M function in 2005 (see BT1700)
+vec3 toLinear2pt8(vec3 _rgb)
+{
+	return pow(_rgb.rgb, vec3_splat(2.8));
+}
+
+vec3 toLinear13thMonkey(vec3 _rgb)
+{
+    // x= ((y + 0.15)^2.2 - (0.15^2.2)) / (1.15^2.2 - 0.15^2.2)
+    // https://www.13thmonkey.org/~boris/gammacorrection/
+    return (pow((_rgb.rgb + vec3_splat(0.15)), vec3_splat(2.2)) - pow(vec3_splat(0.15), vec3_splat(2.2))) / (pow(vec3_splat(1.15), vec3_splat(2.2)) - pow(vec3_splat(0.15), vec3_splat(2.2)));
 }
 
 vec3 toGamma(vec3 _rgb)
