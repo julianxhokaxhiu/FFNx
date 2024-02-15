2cbeda869d85e0f285c34654a6f5500f8c385007
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2024-02-15 22:01:04 +0100

    debug: no upgrade

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -57,7 +57,7 @@ Write-Output "_CHANGELOG_VERSION=${env:_CHANGELOG_VERSION}" >> ${env:GITHUB_ENV}
 
 # Install CMake
 Write-Output "Installing cmake v${env:_WINGET_CMAKE}..."
-winget install Kitware.CMake --version ${env:_WINGET_CMAKE} --silent --accept-source-agreements --accept-package-agreements --disable-interactivity --uninstall-previous
+winget install Kitware.CMake --version ${env:_WINGET_CMAKE} --silent --accept-source-agreements --accept-package-agreements --disable-interactivity --uninstall-previous --no-upgrade
 cmake --version
 
 # Install Powershell
