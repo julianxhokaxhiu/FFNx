julianxhokaxhiu/FFNx

commit a594318ef6775e41092c29d05868694efedc8375

FF7: Expose the Audio FMT header to FFNx

src/sfx.cpp
@@ -42,7 +42,7 @@ void ff7_sfx_release(IDirectSoundBuffer *buffer)
 
 bool ff7_should_sfx_loop(int id)
 {
-	return *(BYTE *)(ff7_externals.sfx_fmt_header + (28 * (id - 1)));
+	return ff7_externals.sfx_fmt_header[id].loop;
 }
 
 void ff7_sfx_stop_channel(int channel, double time = 0)

src/ff7.h
@@ -1662,6 +1662,17 @@ struct dll_gfx_externals
 	gfx_light_polygon_set *generic_light_polygon_set;
 };
 
+struct ff7_audio_fmt
+{
+	uint32_t length;
+	uint32_t offset;
+	uint32_t loop;
+	uint32_t count;
+	uint32_t loop_start;
+	uint32_t loop_end;
+	LPWAVEFORMATEX wave_format;
+};
+
 struct ff7_game_obj
 {
 	uint32_t do_quit;
@@ -2943,7 +2954,7 @@ struct ff7_externals
 	uint32_t *sfx_initialized;
 	uint32_t sfx_play_summon;
 	uint32_t sfx_load_and_play_with_speed;
-	uint32_t sfx_fmt_header;
+	ff7_audio_fmt* sfx_fmt_header;
 	DWORD *sfx_play_effects_id_channel_6;
 	uint32_t sfx_stop_channel_6;
 	UINT *sfx_stop_channel_timer_handle;

src/ff7_data.h
@@ -467,7 +467,7 @@ void ff7_find_externals(struct ff7_game_obj* game_object)
 	ff7_externals.sfx_play_effects_id_channel_6 = (DWORD *)get_absolute_value((uint32_t)common_externals.play_sfx_effects, 0x119);
 	ff7_externals.sound_states = (ff7_field_sfx_state*)get_absolute_value(common_externals.play_sfx_on_channel, 0x28);
 	common_externals.master_sfx_volume = (uint32_t*)get_absolute_value(common_externals.play_sfx_on_channel, 0x342);
-	ff7_externals.sfx_fmt_header = (uint32_t)get_absolute_value(common_externals.play_sfx_on_channel, 0x51);
+	ff7_externals.sfx_fmt_header = (ff7_audio_fmt*)get_absolute_value(common_externals.sfx_load, 0x51);
 
 	common_externals.directsound_create = get_relative_call(common_externals.sfx_init, 0x11C);
 	common_externals.directsound = (LPLPDIRECTSOUND)get_absolute_value(common_externals.directsound_create, 0x15);

commit bbf0e6edcf465958f530c7df46eea9eb98655446

FF7: Fix loop flag lookup for SFX effects
Fixes regression introduced by a594318ef6775e41092c29d05868694efedc8375

src/sfx.cpp
@@ -42,7 +42,7 @@ void ff7_sfx_release(IDirectSoundBuffer *buffer)
 
 bool ff7_should_sfx_loop(int id)
 {
-	return ff7_externals.sfx_fmt_header[id].loop;
+	return ff7_externals.sfx_fmt_header[id-1].loop;
 }
 
 void ff7_sfx_stop_channel(int channel, double time = 0)