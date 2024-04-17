6e6322e818ddf398d4e3733c94f6eae0dd31bc85
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2024-04-17 19:46:38 +0200

    vcpkg: Switch again to hwinfo fork
    Fix crash on startup for certain machines where some PC specs info can't be queried correctly

--- .vcpkg/ports/hwinfo/portfile.cmake
+++ .vcpkg/ports/hwinfo/portfile.cmake
@@ -2,9 +2,9 @@ vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
-    REPO lfreist/hwinfo
-    REF 9ba5dfecb8fb47878c51a18b9b64cf3b5184e7ee
-    SHA512 85346f0e1271bd85724946e0ffb5a3b50bdfb3e597519735641cc5656aae07853a7366019867bbe815157a603d0dd7e2290b1a7ae236a1890abb3ea3ff3232c0
+    REPO julianxhokaxhiu/hwinfo
+    REF c02bca930fb3ebbfc0c3098b6a3ace188d8486c8
+    SHA512 cbb734df915332b931f3db372981591cee7cde678f00a91ca03fce5f7bd72a00c7515e36db198bd6cce40c60b4ac727cdf1f3a5332a865e1f66afbb5e769cd7c
 )
 
 vcpkg_cmake_configure(
