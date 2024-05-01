julianxhokaxhiu/FFNx

commit fb5a8015709cd384a23f6838a2bbcf1fb7f96b9c

FF8: Add ambient layer support
Fixes #688

src/ff8/ambient.cpp
@@ -1,0 +1,64 @@
+/****************************************************************************/
+//    Copyright (C) 2009 Aali132                                            //
+//    Copyright (C) 2018 quantumpencil                                      //
+//    Copyright (C) 2018 Maxime Bacoux                                      //
+//    Copyright (C) 2020 myst6re                                            //
+//    Copyright (C) 2020 Chris Rizzitello                                   //
+//    Copyright (C) 2020 John Pritchard                                     //
+//    Copyright (C) 2024 Julian Xhokaxhiu                                   //
+//                                                                          //
+//    This file is part of FFNx                                             //
+//                                                                          //
+//    FFNx is free software: you can redistribute it and/or modify          //
+//    it under the terms of the GNU General Public License as published by  //
+//    the Free Software Foundation, either version 3 of the License         //
+//                                                                          //
+//    FFNx is distributed in the hope that it will be useful,               //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//    GNU General Public License for more details.                          //
+/****************************************************************************/
+
+#include "ambient.h"
+
+void ff8_handle_ambient_playback()
+{
+	struct game_mode *mode = getmode_cached();
+	static char filename[64]{0};
+	static WORD last_field_id = 0, last_battle_id = 0, is_game_paused = 0;
+
+	switch (mode->driver_mode)
+	{
+	case MODE_BATTLE:
+		if (last_battle_id != *ff8_externals.battle_encounter_id)
+		{
+			last_battle_id = *ff8_externals.battle_encounter_id;
+
+			sprintf(filename, "bat_%d", last_battle_id);
+			nxAudioEngine.playAmbient(filename);
+		}
+		is_game_paused = ff8_externals.check_game_is_paused(NULL) != 0;
+		if (is_game_paused && nxAudioEngine.isAmbientPlaying())
+			nxAudioEngine.pauseAmbient();
+		else if (!is_game_paused && !(nxAudioEngine.isAmbientPlaying()))
+			nxAudioEngine.resumeAmbient();
+		break;
+	case MODE_FIELD:
+		if (last_field_id != *common_externals.current_field_id)
+		{
+			last_field_id = *common_externals.current_field_id;
+
+			sprintf(filename, "field_%d", last_field_id);
+			nxAudioEngine.playAmbient(filename);
+		}
+		break;
+	default:
+		if (last_field_id != 0 || last_battle_id != 0)
+		{
+			nxAudioEngine.stopAmbient();
+			last_field_id = 0;
+			last_battle_id = 0;
+		}
+		break;
+	}
+}

src/common.cpp
@@ -63,6 +63,7 @@
 #include "ff8/vibration.h"
 #include "ff8/engine.h"
 #include "ff8/uv_patch.h"
+#include "ff8/ambient.h"
 
 #include "wine.h"
 
@@ -1221,6 +1222,8 @@ void common_flip(struct game_obj *game_object)
 		if (mode->driver_mode == MODE_CARDGAME) common_clear_all(0);
 
 		if (ff8_ssigpu_debug) ff8_externals.refresh_vram_window();
+
+		ff8_handle_ambient_playback();
 	}
 	else
 	{

src/ff8/ambient.h
@@ -1,0 +1,27 @@
+/****************************************************************************/
+//    Copyright (C) 2009 Aali132                                            //
+//    Copyright (C) 2018 quantumpencil                                      //
+//    Copyright (C) 2018 Maxime Bacoux                                      //
+//    Copyright (C) 2020 myst6re                                            //
+//    Copyright (C) 2020 Chris Rizzitello                                   //
+//    Copyright (C) 2020 John Pritchard                                     //
+//    Copyright (C) 2024 Julian Xhokaxhiu                                   //
+//                                                                          //
+//    This file is part of FFNx                                             //
+//                                                                          //
+//    FFNx is free software: you can redistribute it and/or modify          //
+//    it under the terms of the GNU General Public License as published by  //
+//    the Free Software Foundation, either version 3 of the License         //
+//                                                                          //
+//    FFNx is distributed in the hope that it will be useful,               //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//    GNU General Public License for more details.                          //
+/****************************************************************************/
+
+#pragma once
+
+#include "../audio.h"
+#include "../ff8.h"
+
+void ff8_handle_ambient_playback();

commit da83369296462d3451b848d53f75fa010db69ff8

FF8: Fix crash while closing the game in battle
Fix regression introduced by fb5a8015709cd384a23f6838a2bbcf1fb7f96b9c

src/ff8.h
@@ -1311,6 +1311,7 @@ struct ff8_externals
 	uint32_t stop_cdrom_cleanup_call;
 	uint32_t** savemap;
 	int32_t (*check_game_is_paused)(int32_t);
+	DWORD* is_game_paused;
 	uint32_t sub_470250;
 	uint32_t *ssigpu_callbacks_1;
 	uint32_t *ssigpu_callbacks_2;

src/ff8/ambient.cpp
@@ -25,7 +25,7 @@ void ff8_handle_ambient_playback()
 {
 	struct game_mode *mode = getmode_cached();
 	static char filename[64]{0};
-	static WORD last_field_id = 0, last_battle_id = 0, is_game_paused = 0;
+	static WORD last_field_id = 0, last_battle_id = 0;
 
 	switch (mode->driver_mode)
 	{
@@ -37,10 +37,9 @@ void ff8_handle_ambient_playback()
 			sprintf(filename, "bat_%d", last_battle_id);
 			nxAudioEngine.playAmbient(filename);
 		}
-		is_game_paused = ff8_externals.check_game_is_paused(NULL) != 0;
-		if (is_game_paused && nxAudioEngine.isAmbientPlaying())
+		if ((*ff8_externals.is_game_paused != 0) && nxAudioEngine.isAmbientPlaying())
 			nxAudioEngine.pauseAmbient();
-		else if (!is_game_paused && !(nxAudioEngine.isAmbientPlaying()))
+		else if (!(*ff8_externals.is_game_paused != 0) && !(nxAudioEngine.isAmbientPlaying()))
 			nxAudioEngine.resumeAmbient();
 		break;
 	case MODE_FIELD:

src/ff8_data.cpp
@@ -231,6 +231,7 @@ void ff8_find_externals()
 	ff8_externals.battle_trigger_field = ff8_externals.sub_47CA90 + 0x15;
 	ff8_externals.sub_52B3A0 = (int (*)())get_relative_call(ff8_externals.battle_trigger_field, 0);
 	ff8_externals.check_game_is_paused = (int32_t(*)(int32_t))get_relative_call(ff8_externals.field_main_loop, 0x16C);
+	ff8_externals.is_game_paused = (DWORD*)get_absolute_value((uint32_t)ff8_externals.check_game_is_paused, 0x78);
 	ff8_externals.pause_menu = (int(*)(int))get_relative_call(uint32_t(ff8_externals.check_game_is_paused), 0x88);
 	ff8_externals.init_pause_menu = get_relative_call(uint32_t(ff8_externals.check_game_is_paused), 0xE2);
 	ff8_externals.pause_menu_with_vibration = (int(*)(int))(ff8_externals.init_pause_menu - 0x290);