4311a20ddb508fa66f90cf5f73fccf7ff67abd86
Tangtang Zhou <tangtang2995@gmail.com>
2024-08-19 15:46:07 +0200

    Refactor: use a lower macro value for _WIN32_WINNT

--- src/common.cpp
+++ src/common.cpp
@@ -21,7 +21,7 @@
 //    GNU General Public License for more details.                          //
 /****************************************************************************/
 
-#define _WIN32_WINNT 0x0A00
+#define _WIN32_WINNT 0x0600
 
 #include "renderer.h"
 
