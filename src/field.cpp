555a79219e7f271bf42758b3206f6051b85b8e3b
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2024-08-31 01:46:18 +0200

    DevTools: Ensure the triangleID address is valid before using it

--- src/field.cpp
+++ src/field.cpp
@@ -370,7 +370,7 @@ void field_debug(bool *isOpen)
 
 	ImGui::Text("Game Moment: %u", *common_externals.field_game_moment);
 	ImGui::Text("Current field ID: %u", *common_externals.current_field_id);
-	ImGui::Text("Current triangle ID: %d", *common_externals.current_triangle_id);
+	if (common_externals.current_triangle_id != 0) ImGui::Text("Current triangle ID: %d", *common_externals.current_triangle_id);
 	ImGui::Text("Previous field ID: %u", *common_externals.previous_field_id);
 	ImGui::Separator();
 
