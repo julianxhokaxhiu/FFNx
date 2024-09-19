4d0eef51109207c3d60874b60031dde912e043e8
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2024-09-19 16:56:32 +0200

    FF8: Keep default movie paths as relative to the exe
    Related #732

--- src/movies.cpp
+++ src/movies.cpp
@@ -198,15 +198,8 @@ void ff8_prepare_movie(uint8_t disc, uint32_t movie)
 	_snprintf(fmvName, sizeof(fmvName), "data/movies/disc%02i_%02ih.%s", disc, movie, ffmpeg_video_ext.c_str());
 	_snprintf(camName, sizeof(camName), "data/movies/disc%02i_%02i.cam", disc, movie);
 
-	if (redirect_path_with_override(fmvName, newFmvName, sizeof(newFmvName)) == 0)
-	{
-		_snprintf(newFmvName, sizeof(newFmvName), "%s/data/movies/disc%02i_%02ih.%s", ff8_externals.app_path, disc, movie, ffmpeg_video_ext.c_str());
-	}
-
-	if (redirect_path_with_override(camName, newCamName, sizeof(newCamName)) == 0)
-	{
-		_snprintf(newCamName, sizeof(newCamName), "%s/data/movies/disc%02i_%02i.cam", ff8_externals.app_path, disc, movie);
-	}
+	redirect_path_with_override(fmvName, newFmvName, sizeof(newFmvName));
+	redirect_path_with_override(camName, newCamName, sizeof(newCamName));
 
 	if(trace_all || trace_movies) ffnx_trace("prepare_movie %s disc=%d movie=%d\n", fmvName, disc, movie);
 
