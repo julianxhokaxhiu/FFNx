927f2099eac48cf18da8d696e515cb0f57048909
Julian Xhokaxhiu <info@julianxhokaxhiu.com>
2024-12-11 23:42:18 +0100

    Revert "vcpkg: Ship custom steam_api.dll that doesn't clash with game default one"
    This reverts commit 1b73722d7e53ec5cd4b16dce01a91b524a023368.

--- .vcpkg/ports/steamworkssdk/portfile.cmake
+++ .vcpkg/ports/steamworkssdk/portfile.cmake
@@ -15,55 +15,19 @@ vcpkg_from_github(
 file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}/include/steamworkssdk" INCLUDE_PATH)
 file(TO_NATIVE_PATH "${CURRENT_PACKAGES_DIR}/tools/steamworkssdk" TOOLS_PATH)
 
-file(MAKE_DIRECTORY "${INCLUDE_PATH}")
+file(MAKE_DIRECTORY
+    "${INCLUDE_PATH}"
+)
 
-file(GLOB HEADER_FILES "${SOURCE_PATH}/steamworks_sdk/public/steam/*.h")
+file(GLOB 
+    HEADER_FILES
+    "${SOURCE_PATH}/steamworks_sdk/public/steam/*.h"
+)
 
+file(COPY ${SOURCE_PATH}/steamworks_sdk/redistributable_bin/steam_api.lib DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
+file(COPY ${SOURCE_PATH}/steamworks_sdk/redistributable_bin/steam_api.lib DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
 file(COPY ${SOURCE_PATH}/steamworks_sdk/redistributable_bin/steam_api.dll DESTINATION ${TOOLS_PATH})
-file(RENAME ${TOOLS_PATH}/steam_api.dll ${TOOLS_PATH}/FFNx_steam_api.dll)
 file(COPY ${HEADER_FILES} DESTINATION ${INCLUDE_PATH})
 
 # Copy cmake configuration files
 configure_file(${CMAKE_CURRENT_LIST_DIR}/STEAMWORKSSDKConfig.cmake.in ${CURRENT_PACKAGES_DIR}/share/${PORT}/STEAMWORKSSDKConfig.cmake @ONLY)
-
-################################################
-### Redirect steam_api.dll to FFNx_steam_api.dll
-################################################
-
-set(EXPORTS_FILE "${CURRENT_BUILDTREES_DIR}/exports.txt")
-set(EXPORTS_LOGFILE "${CURRENT_BUILDTREES_DIR}/exports.log")
-set(DEF_FILE "${CURRENT_BUILDTREES_DIR}/FFNx_steam_api.def")
-set(EXP_FILE "${CURRENT_BUILDTREES_DIR}/FFNx_steam_api.exp")
-set(NEW_LIB_FILE "${CURRENT_BUILDTREES_DIR}/FFNx_steam_api.lib")
-
-# Dumpbin to extract exports
-execute_process(
-  COMMAND dumpbin /EXPORTS ${SOURCE_PATH}/steamworks_sdk/redistributable_bin/steam_api.lib
-  OUTPUT_FILE ${EXPORTS_FILE}
-  ERROR_FILE ${EXPORTS_LOGFILE}
-)
-
-# Generate .def file using CMake script logic
-file(WRITE ${DEF_FILE} "LIBRARY FFNx_steam_api.dll\nEXPORTS\n")
-file(STRINGS ${EXPORTS_FILE} EXPORT_LINES REGEX "^                  .+$")
-foreach(EXPORT_LINE ${EXPORT_LINES})
-  string(REGEX MATCH "_(.+)" EXPORT_NAME ${EXPORT_LINE})
-  if(CMAKE_MATCH_1)
-    set(EXPORT_NAME ${CMAKE_MATCH_1})
-    string(STRIP ${EXPORT_NAME} EXPORT_NAME)
-    file(APPEND ${DEF_FILE} "${EXPORT_NAME}\n")
-  endif()
-endforeach()
-
-# Create new .lib file using the .def file
-execute_process(
-  COMMAND lib /machine:x86 /def:${DEF_FILE} /out:${NEW_LIB_FILE}
-  WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
-)
-
-# Replace the original .lib file
-file(COPY ${NEW_LIB_FILE} DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
-file(COPY ${NEW_LIB_FILE} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/lib)
-
-# Post-install actions to clean up
-file(REMOVE_RECURSE ${EXPORTS_FILE} ${EXPORTS_LOGFILE} ${DEF_FILE} ${NEW_LIB_FILE} ${EXP_FILE})
