julianxhokaxhiu/FFNx

commit d5ba3eff9725e4c6a56252d026285274f8c8f02f

Renderer: Fix sudden black frame after new game movie ends

src/renderer.cpp
@@ -463,12 +463,9 @@ void Renderer::renderFrame()
     {
         bool needsToDraw = internalState.bHasDrawBeenDone;
 
-        if (internalState.bHasDrawBeenDone)
-            useTexture(
-                bgfx::getTexture(backendFrameBuffer).idx
-            );
-        else
-            useTexture(0);
+        useTexture(
+            bgfx::getTexture(backendFrameBuffer).idx
+        );
 
         setClearFlags(true, true);
 
@@ -1600,29 +1597,32 @@ void Renderer::show()
     // Reset internal state
     resetState();
 
-    renderFrame();
+    if (internalState.bHasDrawBeenDone)
+    {
+        renderFrame();
 
-    bgfx::update(
-        vertexBufferHandle,
-        0,
-        bgfx::copy(
-            vertexBufferData.data(),
-            vectorSizeOf(vertexBufferData)
-        )
-    );
+        bgfx::update(
+            vertexBufferHandle,
+            0,
+            bgfx::copy(
+                vertexBufferData.data(),
+                vectorSizeOf(vertexBufferData)
+            )
+        );
 
-    bgfx::update(
-        indexBufferHandle,
-        0,
-        bgfx::copy(
-            indexBufferData.data(),
-            vectorSizeOf(indexBufferData)
-        )
-    );
+        bgfx::update(
+            indexBufferHandle,
+            0,
+            bgfx::copy(
+                indexBufferData.data(),
+                vectorSizeOf(indexBufferData)
+            )
+        );
 
-    bgfx::frame(doCaptureFrame);
+        bgfx::frame(doCaptureFrame);
 
-    if (trace_all || trace_renderer) ffnx_trace("Renderer::%s\n", __func__);
+        if (trace_all || trace_renderer) ffnx_trace("Renderer::%s\n", __func__);
+    }
 
     bgfx::dbgTextClear();
 

commit 00660fd61fe0f2c3d3027ad2ee7caf04f19624b8

Renderer: Fix regression introduced by d5ba3eff9725e4c6a56252d026285274f8c8f02f
Preserve removing FF8 sudden black frame, while ensuring FF7 doesn't crash in certain scenes

src/renderer.cpp
@@ -475,23 +475,24 @@ void Renderer::renderFrame()
     backendProgram = RendererProgram::POSTPROCESSING;
     backendViewId++;
     {
-        bool needsToDraw = internalState.bHasDrawBeenDone;
+        if (internalState.bHasDrawBeenDone)
+        {
+            useTexture(
+                bgfx::getTexture(backendFrameBuffer).idx
+            );
 
-        useTexture(
-            bgfx::getTexture(backendFrameBuffer).idx
-        );
+            setClearFlags(true, true);
 
-        setClearFlags(true, true);
+            bindVertexBuffer(vertices, 0, 4);
+            bindIndexBuffer(indices, 6);
 
-        bindVertexBuffer(vertices, 0, 4);
-        bindIndexBuffer(indices, 6);
+            setBlendMode(RendererBlendMode::BLEND_DISABLED);
+            setPrimitiveType();
 
-        setBlendMode(RendererBlendMode::BLEND_DISABLED);
-        setPrimitiveType();
+            draw();
 
-        if (needsToDraw) draw();
-
-        setBlendMode();
+            setBlendMode();
+        }
     }
 };
 
@@ -1616,32 +1617,29 @@ void Renderer::show()
     // Reset internal state
     resetState();
 
-    if (internalState.bHasDrawBeenDone)
-    {
-        renderFrame();
+    renderFrame();
 
-        bgfx::update(
-            vertexBufferHandle,
-            0,
-            bgfx::copy(
-                vertexBufferData.data(),
-                vectorSizeOf(vertexBufferData)
-            )
-        );
+    bgfx::update(
+        vertexBufferHandle,
+        0,
+        bgfx::copy(
+            vertexBufferData.data(),
+            vectorSizeOf(vertexBufferData)
+        )
+    );
 
-        bgfx::update(
-            indexBufferHandle,
-            0,
-            bgfx::copy(
-                indexBufferData.data(),
-                vectorSizeOf(indexBufferData)
-            )
-        );
+    bgfx::update(
+        indexBufferHandle,
+        0,
+        bgfx::copy(
+            indexBufferData.data(),
+            vectorSizeOf(indexBufferData)
+        )
+    );
 
-        bgfx::frame(doCaptureFrame);
+    bgfx::frame(doCaptureFrame);
 
-        if (trace_all || trace_renderer) ffnx_trace("Renderer::%s\n", __func__);
-    }
+    if (trace_all || trace_renderer) ffnx_trace("Renderer::%s\n", __func__);
 
     bgfx::dbgTextClear();
 